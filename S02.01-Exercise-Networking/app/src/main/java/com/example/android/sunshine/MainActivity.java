d877bdf083cd5642708b9612cf6d075b495b86c1
Serdiuk Serhii <serduksergey90@gmail.com>
2018-06-12 02:37:50 +0300

    solve tickets

--- S02.01-Exercise-Networking/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S02.01-Exercise-Networking/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -15,11 +15,11 @@
  */
 package com.example.android.sunshine;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     private TextView mWeatherTextView;
 
