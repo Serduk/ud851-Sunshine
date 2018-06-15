b211689d9f0179b2aa9fdb814c5e42fc55eb10d7
Serdiuk Serhii <serduksergey90@gmail.com>
2018-06-15 03:14:33 +0300

    solve tasks

--- S04.01-Exercise-LaunchNewActivity/app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ S04.01-Exercise-LaunchNewActivity/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -1,9 +1,7 @@
 package com.example.android.sunshine;
 
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
+import android.support.v7.app.AppCompatActivity;
 
 public class DetailActivity extends AppCompatActivity {
 
